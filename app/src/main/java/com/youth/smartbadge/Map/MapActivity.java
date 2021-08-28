e4e72d281cc1944994e6cb78e05fe58626b03600
JJinTae <46085058+JJinTae@users.noreply.github.com>
2021-08-29 02:50:27 +0900

    Update MapActivity.java

--- app/src/main/java/com/youth/smartbadge/Map/MapActivity.java
+++ app/src/main/java/com/youth/smartbadge/Map/MapActivity.java
@@ -37,7 +37,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MapActivity extends AppCompatActivity {
 
-    private String BASE_URL = "http://112.158.50.42:9080";
+    private String BASE_URL = "http://127.0.0.1:9080"; // Your Server IP:Port
     private SharedPreferences appData;
     private RetrofitAPI retrofitAPI;
 
